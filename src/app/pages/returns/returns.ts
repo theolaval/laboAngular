61cf4d4710b1e4f957b0ed4b23f38906f09c4e08
Administrator <Administrator@WIN-J2Q4O23UCCN>
2025-12-16 09:11:50 +0100

    feat: add Returns component with template and styles

---
+++ src/app/pages/returns/returns.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-returns',
+  imports: [],
+  templateUrl: './returns.html',
+  styleUrl: './returns.scss',
+})
+export class Returns {
+
+}
